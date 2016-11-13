da8238f15c1582be82aaddb81350ed4538d14db9
alechas <alexischas@hotmail.com>
2016-11-13 17:42:17 -0300

    Carga Color

--- index.php
+++ index.php
@@ -55,10 +55,25 @@
 <!-- END footer -->
 <script>
 function myFunction() {
-    var x = document.getElementById("patente").value;
-    // document.getElementById("marca").innerHTML = x;
-    //$("#patente") = "XXX";
-    document.getElementById("marca").value = x;
+    // var x = document.getElementById("patente").value;
+    // document.getElementById("marca").value = x;
+// var connection = new ActiveXObject("ADODB.Connection") ;
+
+// var connectionstring="Data Source=<server>;Initial Catalog=<catalog>;User ID=<user>;Password=<password>;Provider=SQLOLEDB";
+
+// connection.Open(connectionstring);
+// var rs = new ActiveXObject("ADODB.Recordset");
+
+// rs.Open("SELECT * FROM table", connection);
+// rs.MoveFirst
+// while(!rs.eof)
+// {
+//    document.write(rs.fields(1));
+//    rs.movenext;
+// }
+
+// rs.close;
+// connection.close; 
 }
 </script>
 </body>
